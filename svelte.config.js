e8ea509dd67129baf887acc5ae330f27f8413319
Sjors Wijsman <sjorswijsman@gmail.com>
2021-06-09 23:26:37 +0200

    Experiment with node adapter instead of vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import vercel from '@sveltejs/adapter-vercel'
+import node from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <body id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel(),
+		adapter: node(),
 	},
 }
 
