3e6880634422942bc7ade92918643aaa89b576d8
Sjors Wijsman <sjorswijsman@gmail.com>
2021-06-09 23:53:10 +0200

    Reverted to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import node from '@sveltejs/adapter-node'
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <body id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node({ out: 'public' }),
+		adapter: vercel(),
 	},
 }
 
