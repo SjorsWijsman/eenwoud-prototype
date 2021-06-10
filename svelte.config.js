2df3663a9a82a08033bfe98ef429c7c9927fc323
Sjors Wijsman <sjorswijsman@gmail.com>
2021-06-10 11:09:45 +0200

    Back to vercel it is

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import node from '@sveltejs/adapter-node'
+import vercel from '@sveltejs/adapter-node'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <body id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node(),
+		adapter: vercel(),
 	},
 }
 
