fef9bfdef8af11821fbcbfd75c3ec32c2203ff84
Sjors Wijsman <sjorswijsman@gmail.com>
2021-06-13 18:01:56 +0200

    Fix for fetch url suddenly broken

--- src/sections/ForestWalk.svelte
+++ src/sections/ForestWalk.svelte
@@ -2,18 +2,22 @@
 	import AudioPlayer from '../components/AudioPlayer.svelte'
 	import TreeGenerator from '../components/TreeGenerator.svelte'
 
+	import { page } from '$app/stores'
 	import { stories, walk } from '$lib/store'
 
-	fetch('resources/data/stories.json')
+	fetch(`http://${$page.host}${$page.path}resources/data/stories.json`)
 		.then((response) => response.json())
 		.then((storiesData) => {
 			stories.set(storiesData)
 			generateWalk()
 		})
+		.catch((error) => {
+			console.log(error)
+			throw new Error(error)
+		})
 
 	// Generate a new walk from selected themes
 	function generateWalk() {
-		console.log('creating new walk')
 		let availableStories = [...$stories]
 		let newWalk = []
 
